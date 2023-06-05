450215f108a0ce3fffeeec78cdd58fb4137f6b20
Kempis <kempisguerrero96@gmail.com>
2023-06-04 23:51:30 -0500

    Characters get method modified to get only one random quote per character.

--- src/routes/characterRoutes.js
+++ src/routes/characterRoutes.js
@@ -8,7 +8,19 @@ router.get('/', async (req, res) => {
     const randomCharacters = await Character.aggregate([
       { $sample: { size: 20 } },
     ]);
-    res.json(randomCharacters);
+
+    const charactersWithRandomQuotes = randomCharacters.map(character => {
+      if (character.quotes && character.quotes.length > 0) {
+        const randomIndex = Math.floor(Math.random() * character.quotes.length);
+        character.quote = character.quotes[randomIndex].quote;
+      } else {
+        character.quote = 'No quote found 😓';
+      }
+      delete character.quotes; // Eliminar el arreglo de citas original
+      return character;
+    });
+
+    res.json(charactersWithRandomQuotes);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Error al obtener los personajes' });
@@ -21,6 +33,25 @@ router.post('/', async (req, res) => {
     const newCharacter = new Character({ name, anime, imageURL });
     await newCharacter.save();
     res.status(201).json(newCharacter);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { imageURL } = req.body;
+    const character = await Character.findByIdAndUpdate(
+      id,
+      { imageURL },
+      { new: true }
+    );
+    if (!character) {
+      return res.status(404).json({ message: 'Character not found' });
+    }
+    res.json(character);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal server error' });
